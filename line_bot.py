6150046d2d319a0516d115716e9f90662e7d222d
asaka-git <iikuni-bakufu@docomo.ne.jp>
2021-02-17 20:04:13 +0000

    helloworld]

--- line_bot.py
+++ line_bot.py
@@ -43,7 +43,7 @@ def talk_ai(word):
     elif word[0]=='Q':
         url = 'https://www.google.co.jp/search'
         response = requests.get(url, params={'q':word})
-        return response.text
+        return response.url
     else:
         apikey='DZZgNzc1RdpkDzIvzfq6ZGKJCZ1QH4LL'
         client=pya3rt.TalkClient(apikey)
